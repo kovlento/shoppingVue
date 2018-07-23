b4504d213a22e7414b3cd6ca874a1fc6f7984644
kovlento <bluskytee@163.com>
2018-07-23 21:45:13 +0800

    add mongoose&koa2

--- service/index.js
+++ service/index.js
@@ -1,18 +1,31 @@
-//引入connect
-const {connect} = require('./database/init.js')
-
-const Koa = require('koa');
-const app = new Koa();
-
-app.use(async(ctx)=>{
-	ctx.body = 'hello world';
-});
-
-app.listen(3000,()=>{
-	console.log('[server] starting at port 3000')
-})
-
+const Koa = require('koa')
+const app = new Koa()
+const mongoose = require('mongoose')
+const {connect , initSchemas} = require('./database/init.js')
+ 
 //立即执行函数
-(async () =>{
+;(async () =>{
     await connect()
+    initSchemas()
+    const User = mongoose.model('User')
+    let oneUser = new User({userName:'jspang13',password:'123456'})
+  
+    oneUser.save().then(()=>{
+        console.log('插入成功')
+        
+    })
+let  users = await  User.findOne({}).exec()
+ 
+console.log('------------------')
+console.log(users)
+console.log('------------------')  
 })()
+ 
+ 
+app.use(async(ctx)=>{
+    ctx.body = '<h1>hello Koa2</h1>'
+})
+ 
+app.listen(3000,()=>{
+    console.log('[Server] starting at port 3000')
+})
--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -1,5 +1,5 @@
 const BASEURL = "https://www.easy-mock.com/mock/5ae2eeb23fbbf24d8cd7f0b6/SmileVue/";
-const url = {
+const URL = {
 	getShoppingMallInfo:BASEURL+'index',
 	getGoodsInfo:BASEURL+'getGoodsInfo'
 }
