9f82c3450ac3e57eccc57e65204d7c9177562cd5
kovlento <bluskytee@163.com>
2018-07-25 23:54:21 +0800

    add registet pages

--- service/index.js
+++ service/index.js
@@ -2,30 +2,29 @@ const Koa = require('koa')
 const app = new Koa()
 const mongoose = require('mongoose')
 const {connect , initSchemas} = require('./database/init.js')
+const Router = require('koa-router')
+
+//引入我们的user.js模块
+let user = require('./appApi/user.js')
+
+//装载所有子路由
+let router = new Router();
+router.use('/user',user.routes())
+
+//加载路由中间件
+app.use(router.routes())
+app.use(router.allowedMethods())
  
-//立即执行函数
-;(async () =>{
+;(async ()=>{
     await connect()
     initSchemas()
-    const User = mongoose.model('User')
-    let oneUser = new User({userName:'jspang13',password:'123456'})
-  
-    oneUser.save().then(()=>{
-        console.log('插入成功')
-        
-    })
-let  users = await  User.findOne({}).exec()
- 
-console.log('------------------')
-console.log(users)
-console.log('------------------')  
 })()
- 
- 
+
+
 app.use(async(ctx)=>{
-    ctx.body = '<h1>hello Koa2</h1>'
+    ctx.body='<h1>Hello Koa2</h1>'
 })
- 
+
 app.listen(3000,()=>{
     console.log('[Server] starting at port 3000')
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import shoppingMall from '@/components/pages/shoppingMall'
+import Register from '@/components/pages/Register'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'shoppingMall',
-      component: shoppingMall
-    }
+    {path: '/',name: 'shoppingMall',component: shoppingMall},
+    {path: '/register',name: 'Register',component: Register}
   ]
 })
