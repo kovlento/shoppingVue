bf9b2883195499877c9868f00263b6c97f757cbe
kovlento <bluskytee@163.com>
2018-08-07 23:58:56 +0800

    service

--- service/index.js
+++ service/index.js
@@ -8,11 +8,13 @@ const cors = require('koa2-cors')
 
 //引入我们的user.js模块
 let user = require('./appApi/user.js')
+let goods = require('./appApi/goods.js')
+
 
 //装载所有子路由
 let router = new Router();
 router.use('/user',user.routes())
-
+router.use('/goods',goods.routes())
 //加载路由中间件
 app.use(router.routes())
 app.use(router.allowedMethods())
